ac6f9f1e11616f4ce4429cee88cb80e5ac926667
kampffrosch94 <johanneskarl.dev@gmail.com>
2019-06-05 14:10:32 +0200

    rust fmt

--- examples/toml/main.rs
+++ examples/toml/main.rs
@@ -2,30 +2,32 @@
 use serde::Deserialize;
 use std::thread::sleep;
 use std::time::Duration;
-use warmy::{Res, SimpleKey, Store, StoreOpt};
 use warmy::toml_impl::Toml;
+use warmy::{Res, SimpleKey, Store, StoreOpt};
 
 #[derive(Debug, Deserialize)]
 struct Config {
-    msg: String,
+  msg: String,
 }
 
 fn main() {
-    let mut store: Store<(), SimpleKey> = Store::new(StoreOpt::default()).unwrap();
-    let ctx = &mut ();
+  let mut store: Store<(), SimpleKey> = Store::new(StoreOpt::default()).unwrap();
+  let ctx = &mut ();
 
-    let resource: Result<Res<Config>, _> = store.get_by(&SimpleKey::from_path("/examples/toml/hello.toml"), ctx, Toml);
+  let resource: Result<Res<Config>, _> = store.get_by(
+    &SimpleKey::from_path("/examples/toml/hello.toml"),
+    ctx,
+    Toml,
+  );
 
-    match resource {
-        Ok(config) => {
-            loop {
-                store.sync(ctx);
+  match resource {
+    Ok(config) => loop {
+      store.sync(ctx);
 
-                println!("The msg is: '{}'", config.borrow().msg);
-                sleep(Duration::from_millis(1000));
-            }
-        }
+      println!("The msg is: '{}'", config.borrow().msg);
+      sleep(Duration::from_millis(1000));
+    },
 
-        Err(e) => eprintln!("{}", e)
-    }
+    Err(e) => eprintln!("{}", e),
+  }
 }
--- src/toml_impl.rs
+++ src/toml_impl.rs
@@ -5,14 +5,14 @@
 //! [`serde::Deserialize`]: https://docs.rs/serde/1.0.85/serde/trait.Deserialize.html
 
 use serde::Deserialize;
-use std::io;
 use std::fmt;
+use std::io;
 use std::path::PathBuf;
 
 use crate::key::Key;
 use crate::load::{Load, Loaded, Storage};
-use toml::{self,from_str};
 use std::fs::read_to_string;
+use toml::{self, from_str};
 
 /// The Toml universal method. Use this with [`Storage::get_by`] or [`Storage::get_proxied_by`] to
 /// benefit from the automatic implementors.
@@ -27,7 +27,7 @@ pub enum TomlError {
   /// The file specified by the key failed to open.
   CannotReadFile(PathBuf, io::Error),
   /// The input key doesn’t provide enough information to open a file.
-  NoKey
+  NoKey,
 }
 
 impl fmt::Display for TomlError {
@@ -39,24 +39,22 @@ impl fmt::Display for TomlError {
         write!(f, "cannot read file {}: {}", path.display(), e)
       }
 
-      TomlError::NoKey => f.write_str("no path key available")
+      TomlError::NoKey => f.write_str("no path key available"),
     }
   }
 }
 
 impl<C, K, T> Load<C, K, Toml> for T
-where K: Key + Into<Option<PathBuf>>,
-      T: 'static + for<'de> Deserialize<'de> {
+where
+  K: Key + Into<Option<PathBuf>>,
+  T: 'static + for<'de> Deserialize<'de>,
+{
   type Error = TomlError;
 
-  fn load(
-    key: K,
-    _: &mut Storage<C, K>,
-    _: &mut C
-  ) -> Result<Loaded<Self, K>, Self::Error> {
+  fn load(key: K, _: &mut Storage<C, K>, _: &mut C) -> Result<Loaded<Self, K>, Self::Error> {
     if let Some(path) = key.into() {
-      let file_content = read_to_string(&path)
-          .map_err(|ioerr| TomlError::CannotReadFile(path, ioerr))?;
+      let file_content =
+        read_to_string(&path).map_err(|ioerr| TomlError::CannotReadFile(path, ioerr))?;
 
       from_str(&file_content)
         .map(Loaded::without_dep)
