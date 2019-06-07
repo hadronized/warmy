09cafd964be64ebcf0b4cbc563e484dfecbbcfee
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2019-06-07 17:33:20 +0200

    Fix toml nits and examples.

--- examples/toml/main.rs
+++ examples/toml/main.rs
@@ -1,10 +1,10 @@
-// this example can be run with 'cargo run --example toml --features toml_impl'
+//! This example can be run with `cargo run --example toml --features toml-impl`.
+
 use serde::Deserialize;
 use std::env;
-use std::path;
 use std::thread::sleep;
 use std::time::Duration;
-use warmy::toml_impl::Toml;
+use warmy::toml::Toml;
 use warmy::{Res, SimpleKey, Store, StoreOpt};
 
 #[derive(Debug, Deserialize)]
@@ -13,17 +13,17 @@ struct Config {
 }
 
 fn main() {
-  let mut store: Store<(), SimpleKey> = Store::new(StoreOpt::default()).unwrap();
-  let ctx = &mut ();
-  // using cargo manifest dir to build our path lets us find our resource
-  // even if the cwd is not in the project root
-  let resource_path = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
-    let mut path = path::PathBuf::from(manifest_dir);
-    path.push("examples/toml/hello.toml");
-    path
+  // using cargo manifest directory to build the path lets us find our resource even if the cwd is
+  // not the project root
+  let store_opt = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+    StoreOpt::default().set_root(manifest_dir)
   } else {
-    path::PathBuf::from("/examples/toml/hello.toml")
+    StoreOpt::default()
   };
+  let resource_path = "/examples/toml/hello.toml";
+
+  let mut store: Store<(), SimpleKey> = Store::new(store_opt).unwrap();
+  let ctx = &mut ();
 
   let resource: Result<Res<Config>, _> =
     store.get_by(&SimpleKey::from_path(resource_path), ctx, Toml);
